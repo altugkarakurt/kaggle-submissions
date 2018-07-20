fb085903fa955e260e105f233ee93479066720cb
Karakurt <altugkarakurt@gmail.com>
2018-07-19 20:24:31 -0400

    Tested a few new ideas for Titanic

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Kaggle Submissions
 
-This repository hosts my submissions for Kaggle competitions. The codes can be tested by downloading the data from competition website to respective folders and running the main scripts.
+This repository hosts my submissions for Kaggle competitions. The codes can be tested by downloading the data from competition website to respective folders and running the scripts. The most successful submissions can be found as `*_best.py`, other scripts are the ideas that are under development.
 
 | Competition | Score | Rank | Description |
 |:-----------:|:-----:|:----:|:-----------:|
-| [Titanic](https://www.kaggle.com/c/titanic) | 0.78468 | 42% | RandomForest with Age qunatization and new isAlone features |
+| [Titanic](https://www.kaggle.com/c/titanic) | 0.78468 | 42% | RandomForest with Age quantization and new isAlone features |
--- titanic/titanic_best.py
+++ titanic/titanic_best.py
@@ -4,9 +4,6 @@ import sklearn
 from sklearn.svm import SVC, LinearSVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LinearRegression
-
-import pdb
 
 """-------------------- UTIL FUNCTIONS -------------------- """
 def evaluate(test_features, test_labels, model):
@@ -17,48 +14,31 @@ def evaluate(test_features, test_labels, model):
     accuracy = (sum(evals) * 1.0)/len(evals)
     return accuracy
 
-# Uses linear regression on the other columns to predict the missing ages
-def fill_age(features):
-	# Find the training and test set for Linear Regression model
-	missing_rows = features.loc[np.isnan(features["Age"]), features.columns]
-	missing_rows = missing_rows.drop(columns=["Age"])
-	known_rows = features.loc[np.isfinite(features["Age"]), features.columns]
-	known_ages = np.array(known_rows["Age"])
-	known_rows = known_rows.drop(columns=["Age"])
-
-	# Initialize and train model
-	clf = LinearRegression()
-	clf.fit(known_rows, known_ages)
-
-	for idx, row in features.iterrows():
-		if(np.isnan(row["Age"])):
-			features.loc[idx, 'Age'] = clf.predict([row[["Pclass", "Sex", "Fare"]].values])
-
-
 def clean_data(features):
 	# Transform "Sex" attribute to numerical ["male", "female"] --> [1, 0]
+	#features.loc[(features['Sex'] == "male"), "Sex"] = 1
+	#features.loc[(features['Sex'] == "female"), "Sex"] = 0
 	features["Sex"] = features["Sex"].map({'female': 1, 'male': 0})
 
+	# Quantize ages as (missing, child, adult) as (-1, 0, 1) 
+	features.loc[np.isnan(features['Age']), "Age"] = -1 # Missing
+	features.loc[(features['Age'] >= 18), "Age"] = 1 # Adult
+	features.loc[(features['Age'] > 0) & (features['Age'] < 18), "Age"] = 0 # Children
+
 	# Replace NaNs with the average of respective column
-	features.loc[np.isnan(features['Fare']), "Fare"] = np.mean(features["Fare"])
+	features.loc[np.isnan(features['Fare']), "Fare"] = np.mean(features["Fare"]) # Missing
 
 	# Replaces SibSb and Parch columns with binary isAlone column
 	isAlone = [1 if(parch+sibsp > 0) else 0
 		for parch, sibsp in zip(features["Parch"], features["SibSp"])]
 	features = features.drop(columns=["Parch", "SibSp"])
-	features.assign(isAlone = isAlone)
-
-	# Quantize ages as (toddler=[0,7), child=[7,18), adult=[18,55), senior) as (-1, 0, 1)
-	fill_age(features)
-	features.loc[(features['Age'] >= 18), "Age"] = 1 # Adult
-	features.loc[(features['Age'] > 0) & (features['Age'] < 18), "Age"] = 0 # Children
-	return features
+	features = features.add(pd.DataFrame({"isAlone":isAlone}))
 
 def test_submission(model, model_name):
 	features = pd.read_csv("test.csv")
 	passenger_ids = np.array(features["PassengerId"])
 	features = features.drop(columns=ignored_columns)
-	features = clean_data(features)
+	clean_data(features)
 	labels = np.zeros_like(passenger_ids)
 
 	for idx, sample in enumerate(features.values):
@@ -76,29 +56,9 @@ features = features.drop(columns=["Survived"])
 
 # Data cleaning
 # TODO: Include Cabin and transform them to numericals
-features = clean_data(features)
+clean_data(features)
 
-"""-------------------- TRY DIFFERENT SKLEARN ALGORITHMS -------------------- """
-### Support Vector Machines
-clf = SVC()
-clf.fit(features, labels)
-train_accuracy = evaluate(features.values, labels, clf)
-print("Training loss of SVM: %1.4f" % (train_accuracy))
-test_submission(clf, "svm")
-
-### Linear Kernel Support Vector Machines
-clf = LinearSVC()
-clf.fit(features, labels)
-train_accuracy = evaluate(features.values, labels, clf)
-print("Training loss of Linear SVM: %1.4f" % (train_accuracy))
-test_submission(clf, "linear_svm")
-
-### Gaussian Naive Bayes
-clf = GaussianNB()
-clf.fit(features, labels)
-train_accuracy = evaluate(features.values, labels, clf)
-print("Training loss of Gaussian Naive Bayes: %1.4f" % (train_accuracy))
-test_submission(clf, "gaussian_naive_bayes")
+"""-------------------- TRAINING & TESTING THE MODEL -------------------- """
 
 ### Gaussian Naive Bayes
 clf = RandomForestClassifier()
